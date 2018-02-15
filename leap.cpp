79b74493174806b9995ae3a9828a5833f2c13611
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-02-15 14:44:10 +0530

    Create leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+class A
+{public:
+    int s;
+ 
+ void get()
+ {
+ cout<<"enter s";
+ cin>>s;
+     if(s%4==0)
+     cout<<"leap year";
+     else
+     cout<<"not leap year";
+     }
+};
+int main()
+{ 
+    A a;
+   a.get();
+   return 0;
+}
